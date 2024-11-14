a9b92825df1defcb09055573a223f8bf25726da1
IA (naruto522ru) <itachi522ru@gmail.com>
2024-11-14 14:29:15 +0300

    Add border hoster's

--- selective_route.sh
+++ selective_route.sh
@@ -29,6 +29,17 @@ else
 fi
 
 export URL_TO_LIST="https://raw.githubusercontent.com/naruto522ru/resolving-public/main/unblock_suite_ip.txt"
+export border_hoster=yes
+if [[ $border_hoster == yes ]]; then
+    export BORDER_hetzner="https://raw.githubusercontent.com/naruto522ru/ipranges/refs/heads/main/hetzner/ipv4_merged.txt"
+    export BORDER_ovh="https://raw.githubusercontent.com/naruto522ru/ipranges/refs/heads/main/ovh/ipv4_merged.txt"
+    export BORDER_digital_ocean="https://raw.githubusercontent.com/naruto522ru/ipranges/refs/heads/main/digitalocean/ipv4_merged.txt"
+    export BORDER_amazon="https://raw.githubusercontent.com/naruto522ru/ipranges/refs/heads/main/amazon/ipv4_merged.txt"
+    export BORDER_amazoncloudfront="https://raw.githubusercontent.com/naruto522ru/ipranges/refs/heads/main/amazoncloudfront/ipv4_merged.txt"
+    export BORDER_akamai="https://raw.githubusercontent.com/naruto522ru/ipranges/refs/heads/main/akamai/ipv4_merged.txt"
+    export BORDER_linode="https://raw.githubusercontent.com/naruto522ru/ipranges/refs/heads/main/linode/ipv4_merged.txt"
+fi
+
 
 function checking_pre_up {
     if [[ ! $(wget -4 --spider --no-check-certificate ${URL_TO_LIST} &>/dev/null) ]]; then
@@ -50,11 +61,55 @@ function checking_pre_up {
         fi
         exit 1
     fi
+    if [[ $border_hoster == yes ]]; then
+        if [[ $LANG == ru_RU.UTF-8 ]]; then
+            echo -e "\e[1;33mВключаем в лист зарубежные хостинг компании.\033[0m"
+        else
+            if [[ $LANG == en_US.UTF-8 ]]; then
+                echo -e "\e[1;33mList foreign hosting companies.\033[0m"
+            fi
+        fi
+        if [[ -n ${BORDER_hetzner} ]]; then
+            echo -e "\e[1;33mHetzner...\033[0m"
+            wget --no-check-certificate -4 -nv -t 5 ${BORDER_hetzner} -O - >> /tmp/selective_list.txt;
+        fi
+
+        if [[ -n ${BORDER_ovh} ]]; then
+            echo -e "\e[1;33mOVH...\033[0m"
+            wget --no-check-certificate -4 -nv -t 5 ${BORDER_ovh} -O - >> /tmp/selective_list.txt;
+        fi
+
+        if [[ -n ${BORDER_digital_ocean} ]]; then
+            echo -e "\e[1;33mDigital Ocean...\033[0m"
+            wget --no-check-certificate -4 -nv -t 5 ${BORDER_digital_ocean} -O - >> /tmp/selective_list.txt;
+        fi
+
+        if [[ -n ${BORDER_amazon} ]]; then
+            echo -e "\e[1;33mAmazon...\033[0m"
+            wget --no-check-certificate -4 -nv -t 5 ${BORDER_amazon} -O - >> /tmp/selective_list.txt;
+        fi
+
+        if [[ -n ${BORDER_amazoncloudfront} ]]; then
+            echo -e "\e[1;33mAmazon CloudFront...\033[0m"
+            wget --no-check-certificate -4 -nv -t 5 ${BORDER_amazoncloudfront} -O - >> /tmp/selective_list.txt;
+        fi
+
+        if [[ -n ${BORDER_akamai} ]]; then
+            echo -e "\e[1;33mAmazon CloudFront...\033[0m"
+            wget --no-check-certificate -4 -nv -t 5 ${BORDER_akamai} -O - >> /tmp/selective_list.txt;
+        fi
+
+        if [[ -n ${BORDER_linode} ]]; then
+            echo -e "\e[1;33mLinode...\033[0m"
+            wget --no-check-certificate -4 -nv -t 5 ${BORDER_linode} -O - >> /tmp/selective_list.txt;
+        fi
+
+    fi
 }
 
 function prepare_list {
     if [[ -f /tmp/selective_list.txt ]]; then
-        cat /tmp/selective_list.txt | grep -E "/32" | sort > /tmp/32.txt
+        cat /tmp/selective_list.txt | grep -E "/32" | sort -u > /tmp/32.txt
         sed '/\/32/d' /tmp/selective_list.txt | sort > /tmp/sum.txt
         rm -fv /tmp/selective_list.txt;
     else
@@ -175,6 +230,9 @@ fi
 if [[ $1 == down ]]; then
     checking_pre_down;
     unset DEVICE_VPN PROTO_VPN URL_TO_LIST;
+    if [[ $border_hoster == yes ]]; then
+        unset BORDER_hetzner BORDER_ovh BORDER_digital_ocean BORDER_amazon BORDER_amazoncloudfront BORDER_akamai BORDER_linode;
+    fi
 fi
 
 exit 0
